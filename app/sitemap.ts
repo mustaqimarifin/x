4730c91a9220ee6c5d6a0a6c1681ae0152d17497
mustaqimarifin <mustaqimarifin@gmail.com>
2023-06-14 02:36:25 +0800

    asstheReturn11

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,15 +1,15 @@
-import { ProjectDatabaseItem, getProjectsDatabase } from "./data";
+import { ProjectDatabaseItem, getProjectsDatabase } from "./data"
 
-export default async function sitemap() {
-  const posts: ProjectDatabaseItem[] = await getProjectsDatabase();
+export default async function sitemap () {
+  const posts: ProjectDatabaseItem[] = await getProjectsDatabase()
 
   posts.map((post) => ({
     url: `https://eff1gy.xyz/projects/${post.pageId}`,
     lastModified: post.date,
-  }));
+  }))
 
   const routes = [
-    "/",
+    "",
     "/about",
     "/posts",
     "/projects",
@@ -19,7 +19,7 @@ export default async function sitemap() {
   ].map((route) => ({
     url: `https://eff1gy.xyz${route}`,
     lastModified: new Date().toISOString().split("T")[0],
-  }));
+  }))
 
-  return [...routes, ...posts];
+  return [...routes, ...posts]
 }
